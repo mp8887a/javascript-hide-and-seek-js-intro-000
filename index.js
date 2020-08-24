a5f6130b81972c7c09a1678b5a8d473f14d0e6a3
Mike Pokalsky <mpoksky1989@gmail.com>
2020-08-24 17:27:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
-  
+
   for (let i = 0; i < upRank.length; i++){
     upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
     }
-    
+
 }
