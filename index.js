2da8b080832fcfef57594571fa0720d7cef5c048
Mike Pokalsky <mpoksky1989@gmail.com>
2020-08-24 17:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ function increaseRankBy(n) {
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }
 }
+
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node');
+  var deepestNode = grandNode.children[0];
+  for (var i = 0; !deepestNode.children[i]; i) {
+    deepestNode = deepestNode.children[0]
+  }
+  return deepestNode;
+}
